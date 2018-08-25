b468d7e7fdcd0cba3d31ab90c433540723338a7d
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2018-08-25 10:51:58 -0400

    added keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://mohamed:password123@ds229835.mlab.com:29835/language-tutor",
+  indicoApiKey: "55dcf3395da7de03608a9721a35697ea",
+  cookieKey: "skldjfhslkjghaslkjhfaskladlfh234sdf",
+};
+
