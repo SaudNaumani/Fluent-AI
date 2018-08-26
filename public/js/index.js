607a08119ff32cb1a557af73da165746c3359540
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2018-08-26 08:36:36 -0400

    added translation

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,14 @@
 import { loadDynamicBannerText } from './components/banner';
 import { initUpdateNavbarOnScroll } from './components/navbar';
-
+import $ from "jquery";
 
 loadDynamicBannerText();
 
+
+ const ask = document.getElementById("ask");
+  ask.addEventListener('click', (e) => {
+    $('#myModal').on('shown.bs.modal', function () {
+      $('#myInput').trigger('focus')
+    })
+    e.preventDefault();
+  });
